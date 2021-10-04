2494c4b98a7089fa119ddcf30a43c47c8d7c0b01
NguyenTrung210402 <91875395+NguyenTrung210402@users.noreply.github.com>
2021-10-04 19:54:28 +0700

    Exercise

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -13,19 +13,52 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	argc--;
-	int testcase[argc],i;
-	for(i=0; i<argc;i++){
-		if(atoi(argv[i+1])==0){
-			argc = i;
-			break;
-		}
-		else
-			testcase[i] = atoi(argv[i+1]);
-	}
-	//Your codes here
 
-	return 0;
+	//Your codes here
+int main(int argc, char *argv[]){
+    int max = -999;
+    int min = 999
+    ;
+
+    if(argc < 2) {
+        return 0;
+    }
+    else if(argc == 2) {
+        int num = atoi(argv[1]);
+        if(num == 0){
+            return 0;
+        }else{
+            max = num;
+            min = num;
+        }
+    }else{
+        for(int i = 1; i < argc; i ++){
+            int num = atoi(argv[i]);
+            if(num == 0){
+                break;
+            }else{
+                if(num > max){
+                    max = num;
+                }
+
+                if(num < min){
+                    min = num;
+                }
+            }
+        }
+    }
+
+    
+
+    printf("Input: ");
+    for(int i = 1; i < argc; i ++){
+        int num = atoi(argv[i]);
+        printf("%d ", num);
+    }
+
+    printf("\nMax: %d", max);
+    printf("\nMin: %d", min);
+    printf("\n");
+
+    return 0;
 }
