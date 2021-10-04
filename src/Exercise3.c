33ad3a240a981d376c1d07c08562a7d448452429
NguyenTrung210402 <91875395+NguyenTrung210402@users.noreply.github.com>
2021-10-05 01:06:09 +0700

    ex

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -10,48 +10,26 @@ Ex:
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <math.h>
 
-bool isPrime(int n)
-{
-    // Neu n < 2 thi khong phai la SNT
-    if (n < 2){
-        return false;
-    }       
-     
-    // Neu n = 2 la SNT
-    if (n == 2){
-        return true;
-    }
-     
-    // Neu n la so chan thi ko phai la SNT
-    if (n % 2 == 0){
-        return false;   
-    }
-     
-    // Lap qua cac so le
-    for (int i = 3; i < (n - 1); i += 2){
-        if (n % i == 0){
-            return false;
-        }   
-    }
-     
-    return true;
-}
-
-int main(int argc, char *argv[]){
-    printf("Input: %s\n", argv[1]);
-    printf("Output: ");
-    int n = atoi(argv[1]);
-
-    for(int i = 2; i <= n; i ++){
-        if(isPrime(i)){
-            printf("%d ", i);
-        }
+int main(int argc, char *argv[]) {
+	//testing variable, applying it to your algorithm for auto-evaluating
+	int testcase = atoi(argv[1]);
+	//Your codes here
+	int i, prime, lim_up, n;
+	 for(n=1; n< lim_up; n++)
+    {
+        prime = 1;
+        for(i=2; i<n; i++)
+            if(n%i == 0)
+            {
+                prime = 0;
+                break;
+            }
+        if(prime)
+            printf(" %d ",n);
     }
 
     printf("\n");
-
     return 0;
 }
