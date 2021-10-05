901b7c1b079490b94d129fdb5ec5466813c4a9db
NguyenTrung210402 <91875395+NguyenTrung210402@users.noreply.github.com>
2021-10-05 15:38:28 +0700

    ex

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -8,42 +8,53 @@ Ex:
 |         Min: -3                    |
 |____________________________________|
 */
-
-#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdio.h>
+int main(int argc, char *argv[]){
+    int max = -999;
+    int min = 999
+    ;
 
-int main(int argc, char *argv[]) {
-	//testing variable, applying it to your algorithm for auto-evaluating
-	argc--;
-	int testcase[argc],i;
-	for(i=0; i<argc;i++){
-		if(atoi(argv[i+1])==0){
-			argc = i;
-			break;
-		}
-		else
-			testcase[i] = atoi(argv[i+1]);
-	}
-	//Your codes here
-int digit, n;
-int min, max;
- digit = n % 10;
- n = n / 10;
- min = max = digit;
- while (n > 0)
-    {
-        digit = n % 10;
-		n = n / 10;
-        if (min > digit) min = digit;
-		if (max < digit) max = digit;
-   }
-   printf("Max digit : %d\n", max);
-   printf("Min digit : %d\n", min);
+    if(argc < 2) {
+        return 0;
+    }
+    else if(argc == 2) {
+        int num = atoi(argv[1]);
+        if(num == 0){
+            return 0;
+        }else{
+            max = num;
+            min = num;
+        }
+    }else{
+        for(int i = 1; i < argc; i ++){
+            int num = atoi(argv[i]);
+            if(num == 0){
+                break;
+            }else{
+                if(num > max){
+                    max = num;
+                }
 
-    do {
-        scanf("%d", &n);
-        printMinMaxDigits(n);
-    } while( n < 0);
+                if(num < min){
+                    min = num;
+                }
+            }
+        }
+    }
+
+    
+
+    //printf("Input: ");
+    for(int i = 1; i < argc; i ++){
+        int num = atoi(argv[i]);
+    //    printf("%d ", num);
+    }
+
+    printf("\nMax: %d", max);
+    printf("\nMin: %d", min);
+    printf("\n");
+
 	return 0;
 }
